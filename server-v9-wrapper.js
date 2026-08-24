99c939d68c7736bfc1126c9f8470deb1a6c5fee8
Merubuki <101968774+Merubuki@users.noreply.github.com>
2026-08-24 18:34:35 +0800

    Add Version 9 server entrypoint

---
+++ server-v9-wrapper.js
@@ -0,0 +1,3 @@
+// Casefile Online Version 9 keeps the proven Version 8 multiplayer/server engine
+// and smart forensic patch while upgrading the browser rendering layer.
+require('./server-v8-wrapper.js');
